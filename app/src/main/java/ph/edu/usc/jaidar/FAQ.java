6ec2d173888c1a0ffd54d5cbe6d591e522983f00
0murice <daphnesurigao@gmail.com>
2025-04-28 19:33:39 +0800

    FAQ

--- app/src/main/java/ph/edu/usc/jaidar/FAQ.java
+++ app/src/main/java/ph/edu/usc/jaidar/FAQ.java
@@ -1,24 +1,44 @@
 package ph.edu.usc.jaidar;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.ListView;
+import android.widget.Spinner;
 
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 
-public class FAQ extends AppCompatActivity {
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_faq);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+    public class FAQ extends AppCompatActivity {
+
+        private ListView listViewFAQ;
+        private FAQAdapter faqAdapter;
+        private ArrayList<FAQItem> faqList;
+
+        @Override
+        protected void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            setContentView(R.layout.activity_faq);
+
+            listViewFAQ = findViewById(R.id.listViewFAQ);
+            faqList = new ArrayList<>();
+
+            // Sample Questions and Answers
+            faqList.add(new FAQItem("How do I send my resume/cover letter?", "You can return items within 30 days."));
+            faqList.add(new FAQItem("How do I track my job applications?", "Shipping usually takes 3-5 business days."));
+            faqList.add(new FAQItem("How do I search for jobs or internships?", "You will receive a tracking link by email."));
+            faqList.add(new FAQItem("How do I reset my password?", "You will receive a tracking link by email."));
+            faqList.add(new FAQItem("How can I contact support for help?", "You will receive a tracking link by email."));
+            faqList.add(new FAQItem("How do I delete my account?", "You will receive a tracking link by email."));
+
+
+            faqAdapter = new FAQAdapter(this, faqList);
+            listViewFAQ.setAdapter(faqAdapter);
+        }
     }
-}
---
+++ app/src/main/java/ph/edu/usc/jaidar/FAQAnswer.java
@@ -0,0 +1,26 @@
+package ph.edu.usc.jaidar;
+
+import android.os.Bundle;
+import android.widget.TextView;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class FAQAnswer extends AppCompatActivity {
+
+    private TextView textViewQuestion;
+    private TextView textViewAnswer;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_faq_answer);
+
+        textViewQuestion = findViewById(R.id.textViewQuestion);
+        textViewAnswer = findViewById(R.id.textViewAnswer);
+
+        String question = getIntent().getStringExtra("question");
+        String answer = getIntent().getStringExtra("answer");
+
+        textViewQuestion.setText(question);
+        textViewAnswer.setText(answer);
+    }
+}
