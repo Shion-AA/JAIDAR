4205cd1d19b9c65789f01528e178eef7ce72b427
shion <23101060@usc.edu.ph>
2025-04-30 20:12:15 +0800

    FIXED. sharedpref now properly gets cleared upon signout.

--- app/src/main/java/ph/edu/usc/jaidar/HomePageActivity.java
+++ app/src/main/java/ph/edu/usc/jaidar/HomePageActivity.java
@@ -1,6 +1,8 @@
 package ph.edu.usc.jaidar;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -29,6 +31,11 @@ public class HomePageActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 mAuth.signOut();
+                SharedPreferences sharedPreferences = getSharedPreferences("userpref", Context.MODE_PRIVATE);
+                SharedPreferences.Editor editor = sharedPreferences.edit();
+                editor.clear();
+                editor.apply();
+
                 Intent intent = new Intent(getApplicationContext(), LandingPageActivity.class);
                 startActivity(intent);
             }
