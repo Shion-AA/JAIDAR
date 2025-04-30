4bfe02ea09c387b23d61ddee2e30fbe8b3506420
shion <23101060@usc.edu.ph>
2025-04-30 19:32:04 +0800

    fully functioning login, and remember-me auto login

--- app/src/main/java/ph/edu/usc/jaidar/MainActivity.java
+++ app/src/main/java/ph/edu/usc/jaidar/MainActivity.java
@@ -1,5 +1,7 @@
 package ph.edu.usc.jaidar;
 
+import android.content.Context;
+import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.util.Log;
@@ -7,42 +9,34 @@ import android.widget.TextView;
 import android.widget.Toast;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.firebase.Firebase;
 import com.google.firebase.auth.FirebaseAuth;
 
 public class MainActivity extends AppCompatActivity {
 
-    FirebaseAuth mAuth;
-    TextView greets;
+    FirebaseAuth mAuth = FirebaseAuth.getInstance();
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        SharedPreferences sharedPreferences = this.getSharedPreferences("userpref", Context.MODE_PRIVATE);
+        String email = sharedPreferences.getString("email", null);
+        String password = sharedPreferences.getString("password", null);
+        if(email != null && password != null){
+            mAuth.signInWithEmailAndPassword(email, password).addOnCompleteListener(task -> {
+                if (task.isSuccessful()) {
+                    Intent intent = new Intent(getApplicationContext(), HomePageActivity.class);
+                    startActivity(intent);
+                } else {
+                    Exception e = task.getException();
+                }
+            });
+        } else {
+            Intent intent = new Intent(this, LandingPageActivity.class);
+            this.startActivity(intent);
+        }
         setContentView(R.layout.activity_main);
-
-        FirebaseAuth mAuth = FirebaseAuth.getInstance();
-
-        String testEmail = "23102463@usc.edu.ph";
-        String testPassword = "test1234";
-        greets = findViewById(R.id.greet);
-        mAuth.createUserWithEmailAndPassword(testEmail, testPassword)
-                .addOnCompleteListener(task -> {
-                    if (task.isSuccessful()) {
-                        greets.setText("Successfully inserted");
-                        Toast.makeText(this, "User created successfully!", Toast.LENGTH_SHORT).show();
-                    } else {
-                        Exception e = task.getException();
-                        if (e != null) {
-                            String message = e.getMessage();
-                            if (message != null && message.contains("email address is already in use")) {
-                                greets.setText("Email already registered");
-                                Toast.makeText(this, "Email already registered.", Toast.LENGTH_SHORT).show();
-                            } else {
-                                greets.setText("Error: " + message + "");
-                                Toast.makeText(this, "Error: " + message, Toast.LENGTH_SHORT).show();
-                            }
-                        }
-                    }
-                });
-
-
     }
 }
