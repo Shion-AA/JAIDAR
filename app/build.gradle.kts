0047b651b1b56797bc04747e369b27cc53858b55
mykroble <mykroble@gmail.com>
2025-05-02 16:30:16 +0800

    sign up

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -35,6 +35,7 @@ android {
 dependencies {
     implementation(platform("com.google.firebase:firebase-bom:33.13.0"))
     implementation("com.google.firebase:firebase-analytics")
+    implementation ("com.google.firebase:firebase-firestore")
     implementation ("com.google.firebase:firebase-auth")
     implementation ("com.google.firebase:firebase-database")
     implementation(libs.appcompat)
